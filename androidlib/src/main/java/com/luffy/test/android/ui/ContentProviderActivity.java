7352fe009c5ebf1da9f5e9558adf099c09bf681f
lvlufei <lvlufei@xiaomi.com>
2020-06-28 14:24:26 +0800

    测试ContentProvider

--- androidlib/src/main/java/com/luffy/test/android/ui/ContentProviderActivity.java
+++ androidlib/src/main/java/com/luffy/test/android/ui/ContentProviderActivity.java
@@ -15,6 +15,18 @@ import butterknife.OnClick;
 
 public class ContentProviderActivity extends BaseActivity {
 
+    private static final String TSM_CLIENT_PROVIDER_FEATURE = "content://com.miui.tsmclient.provider.feature";
+    private static final String TSM_CLIENT_PROVIDER_FEATURE_METHOD = "feature";
+    private static final String TSM_CLIENT_PROVIDER_FEATURE_KEY = "feature_value";
+
+    public static final int FEATURE_SUPPORT_TSMCLIENT = 0x0000001;
+    // 与FEATURE_SUPPORT_NFC匹配支持，其他都不支持
+    public static final int FEATURE_SUPPORT_NFC = 0x0000002;
+    public static final int FEATURE_SUPPORT_UNI_SETTINGS = 1 << 8;
+    public static final int FEATURE_SUPPORT_SCAN_PAY = 1 << 9;
+
+    private int mFeatureCode;
+
     @BindView(R2.id.txt_content)
     TextView txtContent;
 
@@ -25,9 +37,28 @@ public class ContentProviderActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    private void init() {
-        Bundle bundle = getContentResolver().call(Uri.parse("content://com.miui.tsmclient.provider.feature"), "feature", null, null);
-        txtContent.setText("值：" + log(bundle.getInt("feature_value"), 2));
+    private void parseTsmClientFeatureCode() {
+        try {
+            Bundle bundle = getContentResolver().call(
+                    Uri.parse(TSM_CLIENT_PROVIDER_FEATURE),
+                    TSM_CLIENT_PROVIDER_FEATURE_METHOD,
+                    null,
+                    null);
+            // 第一位代表支持tsmclient，第二位代表支持nfc，第八位代表支持智能卡设置，第九位代表支持扫码支付
+            mFeatureCode = bundle.getInt(TSM_CLIENT_PROVIDER_FEATURE_KEY);
+            txtContent.setText("值：" + log(mFeatureCode, 2));
+        } catch (Exception e) {
+            // todo
+            txtContent.setText("值：" + 0);
+        } finally {
+            // todo
+        }
+    }
+
+    // 示例是支持扫码支付，其他类似
+    public boolean isSupportScanPay() {
+        boolean isSupport = (mFeatureCode & FEATURE_SUPPORT_SCAN_PAY) != 0;
+        return isSupport;
     }
 
     /**
@@ -44,7 +75,7 @@ public class ContentProviderActivity extends BaseActivity {
     @OnClick({R2.id.btn})
     public void onViewClicked(View view) {
         if (view.getId() == R.id.btn) {
-            init();
+            parseTsmClientFeatureCode();
         }
     }
 }
