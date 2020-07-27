88242c1b1a79f26eec3587917aad2f7c20380f3c
lvlufei <lvlufei@xiaomi.com>
2020-07-27 10:03:19 +0800

    动态添加view

--- androidlib/src/main/java/com/luffy/test/android/ui/owner/dynamicAddView/DynamicAddViewFragment.java
+++ androidlib/src/main/java/com/luffy/test/android/ui/owner/dynamicAddView/DynamicAddViewFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 
@@ -29,10 +30,15 @@ public class DynamicAddViewFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_dynamic_add_view, container, false);
     }
 
-    @OnClick(R2.id.btn)
-    public void onViewClicked() {
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         mDecorView = mActivity.getWindow().getDecorView().findViewById(android.R.id.content);
         mDynamicView = LayoutInflater.from(mActivity).inflate(R.layout.view_dynamic_add, null);
+    }
+
+    @OnClick(R2.id.btn)
+    public void onViewClicked() {
         displaySearchView();
     }
 
@@ -45,22 +51,55 @@ public class DynamicAddViewFragment extends BaseFragment {
     }
 
     private void displaySearchView() {
-        if (mDecorView == null || mDecorView == null) {
+        if (mDecorView == null || mDecorView == null || mDynamicView.getParent() != null) {
             return;
         }
         mDynamicView.clearAnimation();
-        mDynamicView.setAnimation(AnimationUtils.loadAnimation(mActivity, R.anim.anim_bottom_enter));
+        Animation animation = AnimationUtils.loadAnimation(mActivity, R.anim.anim_bottom_enter);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                hasShowDynamicView = true;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mDynamicView.setAnimation(animation);
         mDecorView.addView(mDynamicView);
-        hasShowDynamicView = true;
+
     }
 
     private void hideSearchView() {
-        if (mDecorView == null || mDecorView == null) {
+        if (mDecorView == null || mDecorView == null || mDynamicView.getParent() == null) {
             return;
         }
         mDynamicView.clearAnimation();
-        mDynamicView.setAnimation(AnimationUtils.loadAnimation(mActivity, R.anim.anim_bottom_exit));
+        Animation animation = AnimationUtils.loadAnimation(mActivity, R.anim.anim_bottom_exit);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                hasShowDynamicView = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mDynamicView.setAnimation(animation);
         mDecorView.removeView(mDynamicView);
-        hasShowDynamicView = false;
     }
 }
--- androidlib/src/main/java/com/luffy/test/android/ui/tsm/feature/FeatureActivity.java
+++ androidlib/src/main/java/com/luffy/test/android/ui/tsm/feature/FeatureActivity.java
@@ -3,7 +3,6 @@ package com.luffy.test.android.ui.tsm.feature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.WindowManager;
 
 import com.luffy.test.android.base.BaseActivity;
 
@@ -17,8 +16,6 @@ public class FeatureActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //透明状态栏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         //绑定布局
         FeatureFragment mFeatureFragment = new FeatureFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
