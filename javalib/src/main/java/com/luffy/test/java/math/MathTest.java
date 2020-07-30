cd0de32df04088d2a402268d618bd9194c13a133
lvlufei <lvlufei@xiaomi.com>
2020-07-30 14:01:44 +0800

    math

---
+++ javalib/src/main/java/com/luffy/test/java/math/MathTest.java
@@ -0,0 +1,10 @@
+package com.luffy.test.java.math;
+
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println(Math.max(1, 10));
+        System.out.println(Math.min(1, 10));
+        System.out.println(Math.abs(-1));
+        System.out.println(Math.log10(10000));
+    }
+}
