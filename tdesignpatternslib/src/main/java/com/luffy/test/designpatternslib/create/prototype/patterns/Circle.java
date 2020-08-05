5e05f59ffee24e7c4f7b99d4ba43cc2fbe241853
lvlufei <lvlufei@xiaomi.com>
2020-08-05 16:32:16 +0800

    原型

---
+++ tdesignpatternslib/src/main/java/com/luffy/test/designpatternslib/create/prototype/patterns/Circle.java
@@ -0,0 +1,28 @@
+package com.luffy.test.designpatternslib.create.prototype.patterns;
+
+/**
+ * Created by lvlufei on 2020-08-05
+ *
+ * @name 圆
+ */
+public class Circle implements Shape {
+    @Override
+    public Object clone() {
+        Circle circle = null;
+        try {
+            circle = (Circle) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("拷贝圆失败!");
+        }
+        return circle;
+    }
+
+    @Override
+    public double countArea(double... parameters) {
+        if (parameters.length == 0) {
+            return 0;
+        }
+        return 3.1415 * Math.pow(parameters[0], 2);
+    }
+
+}
---
+++ tdesignpatternslib/src/main/java/com/luffy/test/designpatternslib/create/prototype/patterns/Triangle.java
@@ -0,0 +1,27 @@
+package com.luffy.test.designpatternslib.create.prototype.patterns;
+
+/**
+ * Created by lvlufei on 2020-08-05
+ *
+ * @name 三角形
+ */
+public class Triangle implements Shape {
+    @Override
+    public Object clone() {
+        Triangle triangle = null;
+        try {
+            triangle = (Triangle) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("拷贝三角形失败!");
+        }
+        return triangle;
+    }
+
+    @Override
+    public double countArea(double... parameters) {
+        if (parameters.length < 2) {
+            return 0;
+        }
+        return parameters[0] * parameters[1] / 2;
+    }
+}
