a0782c264451f152e6dc57e0cbecb0e290a04291
lvlufei <lvlufei@xiaomi.com>
2020-10-21 17:11:23 +0800

    测试：list遍历remove

---
+++ tjavalib/src/main/java/com/luffy/test/java/listRemove/TestListRemove.java
@@ -0,0 +1,36 @@
+package com.luffy.test.java.listRemove;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TestListRemove {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        test1(list);
+        System.out.println(list);
+    }
+
+    private static void test1(List<String> list) {
+        for (String string : list) {
+            if ("1".equals(string) || "3".equals(string) || "5".equals(string)) {
+                list.remove(string);
+            }
+        }
+    }
+
+    private static void test2(List<String> list) {
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String string = iterator.next();
+            if ("1".equals(string) || "3".equals(string) || "5".equals(string)) {
+                iterator.remove();
+            }
+        }
+    }
+}
